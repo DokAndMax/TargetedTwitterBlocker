59b40ce8a2f6ce0b4496c015009fa728a1dfe8aa
Maks <38501262+DokAndMax@users.noreply.github.com>
2025-06-21 16:39:26 +0300

    Update targeted-twitter-blocker.user.js

--- targeted-twitter-blocker.user.js
+++ targeted-twitter-blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Targeted Twitter Blocker
 // @namespace    https://github.com/DokAndMax/TargetedTwitterBlocker
-// @version      1.2
+// @version      1.3
 // @description  Block users based on custom conditions with validation
 // @author       DokAndMax
 // @match        https://twitter.com/*
@@ -12,6 +12,7 @@
 // @grant        GM_addStyle
 // @updateURL    https://raw.githubusercontent.com/DokAndMax/TargetedTwitterBlocker/refs/heads/main/targeted-twitter-blocker.user.js
 // @downloadURL  https://raw.githubusercontent.com/DokAndMax/TargetedTwitterBlocker/refs/heads/main/targeted-twitter-blocker.user.js
+// @require      https://raw.githubusercontent.com/DokAndMax/TargetedTwitterBlocker/refs/heads/main/x-client-transaction-id-generator.js
 // ==/UserScript==
 
 (function() {
@@ -438,9 +439,10 @@
         // Configuration settings and helper functions for API endpoints and parameters
         const config = {
             features: {
-                profile_label_improvements_pcf_label_in_post_enabled: false,
+                rweb_video_screen_enabled: false,
+                payments_enabled: false,
+                profile_label_improvements_pcf_label_in_post_enabled: true,
                 rweb_tipjar_consumption_enabled: true,
-                responsive_web_graphql_exclude_directive_enabled: true,
                 verified_phone_label_enabled: false,
                 creator_subscriptions_tweet_preview_api_enabled: true,
                 responsive_web_graphql_timeline_navigation_enabled: true,
@@ -449,6 +451,9 @@
                 communities_web_enable_tweet_community_results_fetch: true,
                 c9s_tweet_anatomy_moderator_badge_enabled: true,
                 responsive_web_grok_analyze_button_fetch_trends_enabled: false,
+                responsive_web_grok_analyze_post_followups_enabled: true,
+                responsive_web_jetfuel_frame: false,
+                responsive_web_grok_share_attachment_enabled: true,
                 articles_preview_enabled: true,
                 responsive_web_edit_tweet_api_enabled: true,
                 graphql_is_translatable_rweb_tweet_is_translatable_enabled: true,
@@ -456,13 +461,15 @@
                 longform_notetweets_consumption_enabled: true,
                 responsive_web_twitter_article_tweet_consumption_enabled: true,
                 tweet_awards_web_tipping_enabled: false,
+                responsive_web_grok_show_grok_translated_post: false,
+                responsive_web_grok_analysis_button_from_backend: false,
                 creator_subscriptions_quote_tweet_preview_enabled: false,
                 freedom_of_speech_not_reach_fetch_enabled: true,
                 standardized_nudges_misinfo: true,
                 tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled: true,
-                rweb_video_timestamps_enabled: true,
                 longform_notetweets_rich_text_read_enabled: true,
                 longform_notetweets_inline_media_enabled: true,
+                responsive_web_grok_image_annotation_enabled: true,
                 responsive_web_enhance_cards_enabled: false,
             },
             fieldToggles: {
@@ -472,8 +479,8 @@
                 withDisallowedReplyControls: false,
             },
             apiEndpoints: {
-                tweetDetail: "https://x.com/i/api/graphql/iP4-On5YPLPgO9mjKRb2Gg/TweetDetail",
-                following: "https://x.com/i/api/graphql/eWTmcJY3EMh-dxIR7CYTKw/Following",
+                tweetDetail: "https://x.com/i/api/graphql/c9RRUtQyVCoDVtyu4CXG0g/TweetDetail",
+                following: "https://x.com/i/api/graphql/0HRVUaBSRLwHSp3nc4HdYg/Following",
                 blockUser: "https://x.com/i/api/1.1/blocks/create.json",
             }
         };
@@ -500,10 +507,12 @@
 
         // ----------------------
         // Creates request headers required for the API calls
-        function createRequestHeaders() {
+        async function createRequestHeaders(httpMethod, urlPathname) {
+        const xTID = await generateTID();
             return {
                 authorization: authToken,
                 'x-csrf-token': csrfToken,
+                'x-client-transaction-id': xTID,
             };
         }
 
@@ -511,7 +520,7 @@
         // Generic function to perform API requests with support for aborting via signal
         async function apiRequest(url, method = 'GET', body = null) {
             const options = {
-                headers: createRequestHeaders(),
+                headers: await createRequestHeaders(method, new URL(url).pathname),
                 method,
                 credentials: 'include',
                 signal // Add abort signal to the request
@@ -544,7 +553,7 @@
                 withQuickPromoteEligibilityTweetFields: true,
                 withBirdwatchNotes: true,
                 withVoice: true,
-                cursor,
+                ...(cursor !== null ? { cursor } : {}),
             };
 
             const url = `${config.apiEndpoints.tweetDetail}?variables=${encodeURIComponent(JSON.stringify(variables))}&features=${encodeURIComponent(JSON.stringify(config.features))}&fieldToggles=${encodeURIComponent(JSON.stringify(config.fieldToggles))}`;
@@ -577,8 +586,8 @@
                         && entry.content.itemContent.user_results.result
                         && entry.content.itemContent.user_results.result.__typename !== "UserUnavailable")
                 .map(entry => ({
-                screenName: entry.content.itemContent.user_results.result.legacy?.screen_name,
-                isBlocked: entry.content.itemContent.user_results.result.legacy.blocking ?? false,
+                screenName: entry.content.itemContent.user_results.result.core?.screen_name,
+                isBlocked: entry.content.itemContent.user_results.result.relationship_perspectives.blocking ?? false,
             })) || [];
         }
 
